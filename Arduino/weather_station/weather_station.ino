a9dd07864d8bf1ad52767c4ce08a875be2c1b3ab
Rangoow <noe.esp@hotmail.fr>
2020-03-01 18:34:26 +0100

    Update weather_station.ino

--- Arduino/weather_station/weather_station.ino
+++ Arduino/weather_station/weather_station.ino
@@ -1,23 +1,20 @@
-//----------------------------------------Include the NodeMCU ESP8266 Library
-#include <ESP8266WiFi.h>
-//----------------------------------------
-#include "DHT.h"  //--> Include the DHT Sensor Library 
-#define DHTTYPE DHT11 //--> Defines the type of DHT sensor used (DHT11, DHT21, and DHT22), in this project the sensor used is DHT11.
+#include <ESP8266WiFi.h>// Include the NodeMCU ESP8266 Library.
+#include "DHT.h"  // Include the DHT Sensor Library.
 
-#define LEDonBoard 2  //--> Defining an On Board LED, used for indicators when the process of connecting to a wifi router
+#define DHTTYPE DHT11 // Defines the type of DHT sensor used.
+#define LEDonBoard 2  // Defining an On Board LED, used for indicators when the process of connecting to a wifi router.
 
-String apiKey = "C864EPUIEOKZRO8I"; //--> Enter your Write API key from ThingSpeak
 
-//----------------------------------------SSID and Password of your WiFi router
-const char* ssid = "Noue";
-const char* password = "adlanegrosdegun";
-//----------------------------------------
-//----------------------------------------ThingSpeak Server
-const char* server = "api.thingspeak.com";
-//----------------------------------------
+String apiKey = "C864EPUIEOKZRO8I"; // Enter your Write API key from ThingSpeak.
 
-const int DHTPin = D2; //--> The pin used for the DHT11 sensor is Pin D1=Pin 5
-DHT dht(DHTPin, DHTTYPE); //--> Initialize DHT sensor, DHT dht(Pin_used, Type_of_DHT_Sensor);
+
+const char* ssid = "Noue"; // SSID of your WiFi router.
+const char* password = "adlanegrosdegun"; // Passwrd of your WiFi router.
+const char* server = "api.thingspeak.com"; // ThingSpeak Server
+const int DHTPin = D2; // The pin used for the DHT11 sensor is Pin D2
+
+
+DHT dht(DHTPin, DHTTYPE); // Initialize DHT sensor : dht(Pin_used, Type_of_DHT_Sensor);
 
 WiFiClient client;
 
@@ -25,29 +22,25 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   delay(500);
-  dht.begin();  //--> Start reading DHT11 sensors
+  dht.begin();  // Start reading DHT11 sensors datas
   delay(500);
   
-  WiFi.begin(ssid, password); //--> Connect to your WiFi router
+  WiFi.begin(ssid, password); // Connect to your WiFi router
   Serial.println("");
     
-  pinMode(LEDonBoard,OUTPUT); //--> On Board LED port Direction output
-  digitalWrite(LEDonBoard, HIGH); //--> Turn off Led On Board
+  pinMode(LEDonBoard,OUTPUT); // On Board LED port Direction output
+  digitalWrite(LEDonBoard, HIGH); // Turn off Led On Board
   
-  //----------------------------------------Wait for connection
-  Serial.print("Connecting");
+  
+  Serial.print("Connecting"); // Waiting for nodeMCU to connect on wifi
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
-    //----------------------------------------Make the On Board Flashing LED on the process of connecting to the wifi router.
     digitalWrite(LEDonBoard, LOW);
     delay(250);
     digitalWrite(LEDonBoard, HIGH);
-    delay(250);
-    //----------------------------------------
+    delay(250); // while wifi isn't connected make the LED flash each second
   }
-  //----------------------------------------
-  digitalWrite(LEDonBoard, HIGH); //--> Turn off the On Board LED when it is connected to the wifi router.
-  //----------------------------------------If connection successful show IP address in serial monitor
+  digitalWrite(LEDonBoard, HIGH); // When connected turn of the light
   Serial.println("");
   Serial.print("Successfully connected to : ");
   Serial.println(ssid);
@@ -55,15 +48,15 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  float h = dht.readHumidity(); // h catch the humitidy value
+  float t = dht.readTemperature(); // t take the temperature value
       
   if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
+    Serial.println("Failed to read from DHT sensor!"); // in case the value is equal to NaN display an error message
     return;
   }
 
-  if (client.connect(server,80))  {   //--> "184.106.153.149" or api.thingspeak.com  
+  if (client.connect(server,80))  {
     String postStr = apiKey;
     postStr +="&field1=";
     postStr += String(t);
@@ -83,18 +76,18 @@ void loop() {
  
     Serial.print("Temperature: ");
     Serial.print(t);
-    Serial.print(" degrees Celcius, Humidity: ");
+    Serial.print(" °C || Humidity: ");
     Serial.print(h);
-    Serial.println("%. Send to Thingspeak.");
+    Serial.println("%.")
+    Serial.println("Datas has been sent to Thingspeak server.");
   }
   client.stop();
-  Serial.println("Waiting...");
+  Serial.println("Waiting...");// waiting for next data set
+  // Even if data are printed in serial monitor each second, ThingSpeak needs 15 sec between each updates
   
-  // thingspeak needs minimum 15 sec delay between updates
-  //----------------------------------------The on board blinks as an indicator that the program is running.
+
   digitalWrite(LEDonBoard, LOW);
   delay(250);
   digitalWrite(LEDonBoard, HIGH);
-  delay(750);
-  //----------------------------------------
+  delay(750);// Make the board LED falshing to show the programm is running.
 }
